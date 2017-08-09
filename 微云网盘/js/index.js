fd9a7012fc080a71532bb0ce71ff457f3a2c5b2c
shenxinze <uidshen@163.com>
2017-08-09 17:30:14 +0800

    微云网盘布局以及初步的交互效果实现

---
+++ 微云网盘/js/index.js
@@ -0,0 +1,21 @@
+/*
+* @Author: shen
+* @Date:   2017-08-08 12:08:33
+* @Last Modified by:   shen
+* @Last Modified time: 2017-08-08 14:07:16
+*/
+(function(){
+/**/	
+var content = document.querySelector('.content');
+var contentLeft = document.querySelector('.content .left');
+var topInfo = document.querySelector('.topInfo');
+var menuBox = document.querySelector('.menuBox');
+content.style.height = window.innerHeight - topInfo.offsetHeight - menuBox.offsetHeight + 'px';
+
+
+
+
+
+
+/**/
+})()
