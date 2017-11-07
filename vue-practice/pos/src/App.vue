43729b697ff7d8bcae7986741d05fa8447164647
shenxinze <uidshen@163.com>
2017-11-07 23:03:55 +0800

    vue-cli 搭建

---
+++ vue-practice/pos/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <i class="icon iconfont icon-dianpu"></i>
+        <i class="icon iconfont icon-shouji"></i>        
+        <router-view/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app'
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
